c490fbc82438a099b37e2a1a3683166991d4f915
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2023-08-15 17:04:25 -0400

    whats up with the server

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const corsOptions = {
   origin: "https://frontend-mapbox-8rktlre2c-pjf1822.vercel.app",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true, // This allows the session cookie to be sent back and forth
-  optionsSuccessStatus: 204, // Some legacy browsers choke on 204
+  optionsSuccessStatus: 204,
 };
 
 // middleware
