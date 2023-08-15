e127028513392ab1e70ef2196019e71848299430
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2023-08-15 14:29:43 -0400

    updated vercel domain

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 const corsOptions = {
-  origin: "https://frontend-mapbox-9xb2wlowd-pjf1822.vercel.app",
+  origin: "https://frontend-mapbox-8rktlre2c-pjf1822.vercel.app/",
 };
 
 // middleware
